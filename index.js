f84dca19a083f765ac288fadb697e23892c9c06e
markuslehtonen <37828663+markuslehtonen@users.noreply.github.com>
2020-10-13 22:24:11 +0300

    Update index.js
    Fix more typos

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ exports.handleStorageUpload = functions.storage.object().onFinalize(async (objec
     const file = bucket.file(object.name);
     const contentType = object.contentType;
 
-    // Remember to return null to avoid time out..
+    // Remember to return null to avoid time out.
     if (!contentType.startsWith('image/')) {
-        functions.logger.log('Uploaded file as not an image.')
+        functions.logger.log('Uploaded file is not an image.')
         return null;
     }
 
     // Remember to return a promise.
     return file.getMetaData()
         .then(metaData => {
-            functions.logger.log('Uploaded image metadata JSON:', JSON.stringify(metaData));
+            functions.logger.log('Uploaded image metadata in JSON:', JSON.stringify(metaData));
         })
         .catch(error => {
             functions.logger.log('Failed at retrieving image metadata, error:', error);
